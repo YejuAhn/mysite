fc450de2b899b9fcdf3a58d9d2c438912b37a788
Yeju Ahn <yejuahn@Jessicas-MacBook-Air.local>
2020-05-15 18:41:37 +0900

    installed rest-framework

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #thirdparty
+    'rest_framework',
 
     #own
     'products',
--- mysite/urls.py
+++ mysite/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
@@ -33,6 +34,8 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('Blog/', include('Blog.urls')),
     path('polls/', include('polls.urls')),
+    url(r'^api-auth/', include('rest_framework.urls'))
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
