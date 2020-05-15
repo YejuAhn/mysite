9236bb72ca8d05cb5d065a9615064a71d0b26804
Yeju Ahn <yejuahn@Jessicas-MacBook-Air.local>
2020-05-15 18:47:03 +0900

    added serializer to Article

--- Blog/serializer.py
+++ Blog/serializer.py
@@ -2,3 +2,15 @@ from rest_framework import serializers
 from .models import Article
 
 
+class BlogSerializer(serializers.ModelSerizlier):
+    class Meta:
+        model = Article
+        fields = ('pk', 'title', 'url', 'description', 'author', 'text', 'pub_date')
+
+        def __str__(self):
+            return self.title
+
+
+
+
+
